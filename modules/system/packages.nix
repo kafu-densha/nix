2278cde4a1e6b8b520d6b6a01b6f7c74555b3d6e
kafu-densha <git@kafu.observer>
2025-12-24 22:02:52 -0800

    Add nix store cleaning utility

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -15,5 +15,11 @@
     bat
   ];
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+  };
+
   environment.variables.EDITOR = "vim";
 }
