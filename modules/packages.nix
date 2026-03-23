097197f7af65632543cfe04feef308b3dc709e75
kafu-densha <git@kafu.observer>
2026-03-22 17:28:28 -0700

    change nixos config path

--- modules/packages.nix
+++ modules/packages.nix
@@ -27,7 +27,7 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/etc/nixos";
+    flake = "/home/elenah/.nixos";
   };
 
   programs.tmux.enable = true;
