ac762e667de89ba08b2c2b84b7b5aabf7f052a13
kafu-densha <git@kafu.observer>
2026-03-24 00:29:23 -0700

    add colmena

--- modules/system.nix
+++ modules/system.nix
@@ -19,4 +19,25 @@
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = pubkeys;
   };
+  
+  # fix colmena apply needing interactive sudo password entry
+  security.sudo.extraRules = [
+    {
+      users = [ "elenah" ];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/nix-store --no-gc-warning --realise /nix/store/*";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/run/current-system/sw/bin/nix-env --profile /nix/var/nix/profiles/system --set /nix/store/*";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/nix/store/*/bin/switch-to-configuration *";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 }
