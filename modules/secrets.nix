0fc765a768e6f90ef4e1fdc31d1839d0e8b5b415
kafu-densha <git@kafu.observer>
2026-03-27 03:07:29 -0700

    add zfs key to agenix

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -9,7 +9,7 @@
 let
   hostkeys = {
     ronri = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ46UqcVxkdL8TeUiZBID7Tz3wjVhPw1SstvfH1hjyrR";
-    ito = ""; # TODO add ito hostkey
+    ito = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILhtJOUxFnQicln/5h268GjBZbrRmFBv7xpa/nZ0JNwe";
   };
 in
 {
@@ -19,6 +19,7 @@ in
     hostPubkey = hostkeys.${config.networking.hostName};
     masterIdentities = [
       ../secrets/master-identities/yubikey-primary.pub
+      ../secrets/master-identities/yubikey-backup.pub
     ];
     storageMode = "local";
     localStorageDir = inputs.self + "/secrets/rekeyed/${config.networking.hostName}";
