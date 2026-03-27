10d358706beffd9338c6d07d41ab327a49f9fb18
kafu-densha <git@kafu.observer>
2026-03-27 00:05:53 -0700

    add zfs support to ito

--- hosts/ito/default.nix
+++ hosts/ito/default.nix
@@ -19,6 +19,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Enable ZFS support
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.forceImportRoot = false;
+  boot.zfs.extraPools = [ ]; # auto import these pools on boot
+
   # allow unfree packages for nvidia
   nixpkgs.config.allowUnfree = true;
 
