d309cced872aa121c6be4457f1d5373e1e8887b2
kafu-densha <git@kafu.observer>
2026-04-01 16:09:23 -0700

    temp remove wallpaper engine

--- hosts/ito/home.nix
+++ hosts/ito/home.nix
@@ -8,13 +8,13 @@
   ];
 
   # wallpaper engine
-  services.linux-wallpaperengine = {
-    enable = true;
-    wallpapers = [
-      {
-        wallpaperId = "2530108698";
-        monitor = "DP-3";
-      }
-    ];
-  };
+  # services.linux-wallpaperengine = {
+  #   enable = true;
+  #   wallpapers = [
+  #     {
+  #       wallpaperId = "2530108698";
+  #       monitor = "DP-3";
+  #     }
+  #   ];
+  # };
 }
