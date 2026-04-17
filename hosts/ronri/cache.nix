fcbdd33a4f4eae99baaf716f20a177c6dedde413
kafu-densha <git@kafu.observer>
2026-04-17 09:31:09 -0700

    switch to attic cache

--- hosts/ronri/cache.nix
+++ hosts/ronri/cache.nix
@@ -1,32 +1,59 @@
 {
   config,
+  pkgs,
+  inputs,
   ...
 }:
 
 let
   publicURL = "nixcache.elenahaug.com";
+  atticPkgs = inputs.attic.packages.${pkgs.system};
 in
 {
   imports = [ ];
 
-  age.secrets.harmonia-secret.rekeyFile = ../../secrets/harmonia-secret.age;
-
-  services.harmonia = {
+  age.secrets.atticd-credentials.rekeyFile = ../../secrets/atticd-credentials.age;
+  services.atticd = {
     enable = true;
-    signKeyPaths = [ config.age.secrets.harmonia-secret.path ];
+    package = atticPkgs.attic-server;
+    environmentFile = config.age.secrets.atticd-credentials.path;
+    settings = {
+      listen = "[::]:8080";
+      api-endpoint = "https://${publicURL}/";
+      database.url = "sqlite:///var/lib/atticd/server.db?mode=rwc";
+      storage = {
+        type = "local";
+        path = "/var/lib/atticd/storage";
+      };
+      chunking = {
+        nar-size-threshold = 65536;
+        min-size = 16384;
+        avg-size = 65536;
+        max-size = 262144;
+      };
+      compression = {
+        type = "zstd";
+      };
+      garbage-collection = {
+        interval = "12 hours";
+        default-retention-period = "6 months";
+      };
+    };
   };
 
   services.nginx.virtualHosts."${publicURL}" = {
     useACMEHost = "elenahaug.com";
     forceSSL = true;
     locations."/".extraConfig = ''
-      proxy_pass http://127.0.0.1:5000;
+      proxy_pass http://127.0.0.1:8080;
       proxy_set_header Host $host;
       proxy_redirect http:// https://;
       proxy_http_version 1.1;
       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
       proxy_set_header Upgrade $http_upgrade;
       proxy_set_header Connection $connection_upgrade;
+
+      client_max_body_size 0;
     '';
   };
 
