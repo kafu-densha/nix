5ba0060af15df29d8bcde749ec2b59ca0de6b258
kafu-densha <git@kafu.observer>
2026-05-25 18:05:39 -0700

    add ocf server & desktop configs (#19)

---
+++ hosts/ocf/common.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+{
+  home.username = "elenah";
+  home.homeDirectory = "/home/b/bl/elenah";
+
+  services.ssh-agent.enable = true;
+
+  programs.ssh.matchBlocks."*".identityFile = lib.mkForce "~/.ssh/id_ed25519_sk.pub";
+
+  programs.git.settings.user.signingkey =
+    lib.mkForce "${config.home.homeDirectory}/.ssh/id_ed25519_sk.pub";
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
+}
