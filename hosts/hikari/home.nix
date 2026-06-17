a4d13927b2bedbf69586d7a49b458205153ab60a
kafu-densha <git@kafu.observer>
2026-06-17 14:57:00 -0700

    add kiro cli zshrc (#36)

--- hosts/hikari/home.nix
+++ hosts/hikari/home.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   ...
 }:
 
@@ -35,5 +36,15 @@ in
     SecurityKeyProvider ${openssh-sk-standalone}/lib/sk-libfido2.dylib
   '';
 
-  # home.sessionVariables.SSH_SK_PROVIDER = "${openssh-sk-standalone}/lib/sk-libfido2.dylib";
+  # amazon kiro cli
+  programs.zsh.initContent = lib.mkMerge [
+    (lib.mkBefore ''
+      # Kiro CLI pre block. Keep at the top of this file.
+      [[ -f "/Users/elenah/Library/Application Support/kiro-cli/shell/zshrc.pre.zsh" ]] && builtin source "/Users/elenah/Library/Application Support/kiro-cli/shell/zshrc.pre.zsh"
+    '')
+    (lib.mkAfter ''
+      # Kiro CLI post block. Keep at the bottom of this file.
+      [[ -f "/Users/elenah/Library/Application Support/kiro-cli/shell/zshrc.post.zsh" ]] && builtin source "/Users/elenah/Library/Application Support/kiro-cli/shell/zshrc.post.zsh"
+    '')
+  ];
 }
