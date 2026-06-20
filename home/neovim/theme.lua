fcd78742f41315d352c8fa154afacb79bbec60d3
kafu-densha <git@kafu.observer>
2026-06-19 17:06:50 -0700

    update nvim theme

--- home/neovim/theme.lua
+++ home/neovim/theme.lua
@@ -12,16 +12,16 @@ vim.o.termguicolors = true
 vim.g.colors_name = "kaf_dark"
 
 local colors = {
-  bg       = "#15142f",
-  fg       = "#f0ebff",
-  black    = "#15142f",
-  red      = "#e36981",
-  green    = "#8fbf9f",
-  yellow   = "#e8c547",
-  blue     = "#7b6fd9",
-  magenta  = "#e070b8",
-  cyan     = "#9aaee8",
-  white    = "#c4c3ca",
+  bg         = "#15142f",
+  fg         = "#f0ebff",
+  black      = "#15142f",
+  red        = "#e36981",
+  green      = "#8fbf9f",
+  yellow     = "#e8c547",
+  blue       = "#7b6fd9",
+  magenta    = "#e070b8",
+  cyan       = "#9aaee8",
+  white      = "#c4c3ca",
   br_black   = "#646378",
   br_red     = "#ff8095",
   br_green   = "#a8d4b6",
@@ -34,16 +34,16 @@ local colors = {
 }
 
 -- Terminal Colors
-vim.g.terminal_color_0 = colors.black
-vim.g.terminal_color_1 = colors.red
-vim.g.terminal_color_2 = colors.green
-vim.g.terminal_color_3 = colors.yellow
-vim.g.terminal_color_4 = colors.blue
-vim.g.terminal_color_5 = colors.magenta
-vim.g.terminal_color_6 = colors.cyan
-vim.g.terminal_color_7 = colors.white
-vim.g.terminal_color_8 = colors.br_black
-vim.g.terminal_color_9 = colors.br_red
+vim.g.terminal_color_0  = colors.black
+vim.g.terminal_color_1  = colors.red
+vim.g.terminal_color_2  = colors.green
+vim.g.terminal_color_3  = colors.yellow
+vim.g.terminal_color_4  = colors.blue
+vim.g.terminal_color_5  = colors.magenta
+vim.g.terminal_color_6  = colors.cyan
+vim.g.terminal_color_7  = colors.white
+vim.g.terminal_color_8  = colors.br_black
+vim.g.terminal_color_9  = colors.br_red
 vim.g.terminal_color_10 = colors.br_green
 vim.g.terminal_color_11 = colors.br_yellow
 vim.g.terminal_color_12 = colors.br_blue
@@ -52,7 +52,7 @@ vim.g.terminal_color_14 = colors.br_cyan
 vim.g.terminal_color_15 = colors.br_white
 
 local highlights = {
-  -- Base Highlights
+  -- Original Base UI Highlights
   Normal       = { fg = colors.fg, bg = colors.bg },
   NormalFloat  = { fg = colors.fg, bg = colors.bg },
   Cursor       = { fg = colors.bg, bg = colors.selection },
@@ -69,7 +69,7 @@ local highlights = {
   Pmenu        = { fg = colors.fg, bg = colors.br_black },
   PmenuSel     = { fg = colors.bg, bg = colors.selection },
   
-  -- Syntax Highlighting
+  -- Traditional Vim Syntax Fallbacks
   Comment      = { fg = colors.br_black, italic = true },
   Constant     = { fg = colors.magenta },
   String       = { fg = colors.green },
@@ -80,9 +80,9 @@ local highlights = {
   Identifier   = { fg = colors.fg },
   Function     = { fg = colors.blue },
   Statement    = { fg = colors.red },
-  Conditional  = { fg = colors.red },
-  Repeat       = { fg = colors.red },
-  Label        = { fg = colors.red },
+  Conditional  = { fg = colors.br_red, italic = true },
+  Repeat       = { fg = colors.br_red, italic = true },
+  Label        = { fg = colors.br_red },
   Operator     = { fg = colors.cyan },
   Keyword      = { fg = colors.red },
   Exception    = { fg = colors.br_red },
@@ -110,6 +110,68 @@ local highlights = {
   DiagnosticWarn  = { fg = colors.yellow },
   DiagnosticInfo  = { fg = colors.blue },
   DiagnosticHint  = { fg = colors.cyan },
+
+  -- Tree-sitter Granular Scopes (Neovim 0.9+)
+  ["@comment"]               = { fg = colors.br_black, italic = true },
+  ["@comment.documentation"] = { fg = colors.br_green, italic = true },
+  
+  ["@keyword"]               = { fg = colors.red },
+  ["@keyword.control"]       = { fg = colors.br_red, italic = true },
+  ["@keyword.control.return"]= { fg = colors.br_red, italic = true },
+  ["@keyword.coroutine"]     = { fg = colors.br_red, italic = true },
+  ["@keyword.operator"]      = { fg = colors.cyan },
+  ["@keyword.function"]      = { fg = colors.red },
+  ["@keyword.type"]          = { fg = colors.red },
+  ["@keyword.modifier"]      = { fg = colors.br_red },
+  
+  ["@function"]              = { fg = colors.blue },
+  ["@function.method"]       = { fg = colors.br_blue },
+  ["@function.macro"]        = { fg = colors.magenta },
+  ["@function.builtin"]      = { fg = colors.br_magenta },
+  
+  ["@type"]                  = { fg = colors.yellow },
+  ["@type.builtin"]          = { fg = colors.br_yellow },
+  ["@type.enum.variant"]     = { fg = colors.br_yellow },
+  ["@type.interface"]        = { fg = colors.yellow, italic = true },
+  
+  ["@constructor"]           = { fg = colors.yellow },
+  
+  ["@variable"]              = { fg = colors.fg },
+  ["@variable.builtin"]      = { fg = colors.magenta, italic = true },
+  ["@variable.parameter"]    = { fg = colors.br_cyan },
+  ["@variable.member"]       = { fg = colors.cyan },
+  
+  ["@property"]              = { fg = colors.cyan },
+  ["@attribute"]             = { fg = colors.br_yellow },
+  ["@label"]                 = { fg = colors.br_red },
+  
+  ["@module"]                = { fg = colors.white, italic = true },
+  ["@namespace"]             = { fg = colors.white, italic = true },
+  
+  ["@string"]                = { fg = colors.green },
+  ["@string.escape"]         = { fg = colors.br_cyan },
+  ["@string.regexp"]         = { fg = colors.br_green },
+  ["@string.special"]        = { fg = colors.br_green },
+  ["@string.special.symbol"] = { fg = colors.yellow },
+  
+  ["@number"]                = { fg = colors.br_magenta },
+  ["@number.float"]          = { fg = colors.br_magenta },
+  ["@boolean"]               = { fg = colors.br_magenta },
+  
+  ["@constant"]              = { fg = colors.magenta },
+  ["@constant.builtin"]      = { fg = colors.br_magenta },
+  
+  ["@operator"]              = { fg = colors.cyan },
+  ["@punctuation.delimiter"] = { fg = colors.br_black },
+  ["@punctuation.bracket"]   = { fg = colors.fg },
+  ["@punctuation.special"]   = { fg = colors.magenta },
+  
+  ["@tag"]                   = { fg = colors.cyan },
+  ["@tag.attribute"]         = { fg = colors.blue },
+  ["@tag.delimiter"]         = { fg = colors.br_black },
+  
+  ["@markup.link.label"]     = { fg = colors.br_blue, italic = true },
+  ["@markup.link.url"]       = { fg = colors.blue, italic = true },
 }
 
 for group, settings in pairs(highlights) do
