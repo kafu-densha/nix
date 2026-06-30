e8aa5ef577cd9ea690bdff681ae3f2f3ccec7855
kafu-densha <git@kafu.observer>
2026-06-30 16:25:47 -0700

    move insecure packages to central location

--- modules/pkgs-config.nix
+++ modules/pkgs-config.nix
@@ -14,4 +14,11 @@ in
 {
   nixpkgs.overlays = [ unstable-packages ];
   nixpkgs.config.allowUnfree = true;
+
+  nixpkgs.config.permittedInsecurePackages = [
+    # TODO remove once bitwarden-desktop has updated electron
+    # https://github.com/NixOS/nixpkgs/issues/526914
+    # https://github.com/bitwarden/clients/pull/20448
+    "electron-39.8.10"
+  ];
 }
