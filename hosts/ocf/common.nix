7c5cec1582989ec0ff77f738301c1ec95eae341d
kafu-densha <git@kafu.observer>
2026-07-01 19:48:25 -0700

    disable ssh agent on ocf machines

--- hosts/ocf/common.nix
+++ hosts/ocf/common.nix
@@ -11,7 +11,7 @@
   home.username = "elenah";
   home.homeDirectory = "/home/b/bl/elenah";
 
-  services.ssh-agent.enable = true;
+  services.ssh-agent.enable = lib.mkForce false;
 
   programs.ssh.settings."*".identityFile = lib.mkForce "~/.ssh/id_ed25519_sk";
 
@@ -22,6 +22,6 @@
     EDITOR = "nvim";
     VISUAL = "nvim";
     SKIP_OCF_ZSHRC = "1";
-    SSH_ASKPASS = "${pkgs.kdePackages.ksshaskpass.out}/bin/ksshaskpass";
+    # SSH_ASKPASS = "${pkgs.kdePackages.ksshaskpass.out}/bin/ksshaskpass";
   };
 }
