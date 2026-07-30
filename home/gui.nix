1680bcb930f58955a84b182363e8c0c3a340f98c
kafu-densha <git@kafu.observer>
2026-07-30 16:53:09 -0700

    add wezterm (#56)

---
+++ home/gui.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  ...
+}:
+
+{
+  programs.wezterm = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      color_scheme = "kafDark";
+      font = lib.generators.mkLuaInline ''wezterm.font("MesloLGS NF")'';
+      hide_tab_bar_if_only_one_tab = false;
+      window_decorations = "INTEGRATED_BUTTONS|RESIZE";
+      font_size = 13;
+      window_background_opacity = 0.98;
+    };
+    colorSchemes = {
+      kafDark = {
+        ansi = [
+          "#15142f"
+          "#e36981"
+          "#8fbf9f"
+          "#e8c547"
+          "#7b6fd9"
+          "#e070b8"
+          "#9aaee8"
+          "#c4c3ca"
+        ];
+        brights = [
+          "#646378"
+          "#ff8095"
+          "#a8d4b6"
+          "#f5d76e"
+          "#9d8fff"
+          "#ff8fd0"
+          "#bcc4ff"
+          "#ffffff"
+        ];
+        background = "#15142f";
+        cursor_bg = "#e19dce";
+        cursor_border = "#e19dce";
+        cursor_fg = "#15142f";
+        foreground = "#f0ebff";
+        selection_bg = "#e19dce";
+        selection_fg = "#15142f";
+      };
+    };
+  };
+}
