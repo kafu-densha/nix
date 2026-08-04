8fe2c1c33fe2e9c6cffd2f8c8ab5e395d91b1e15
kafu-densha <git@kafu.observer>
2026-08-03 21:11:30 -0700

    Revert "disallow insecure electron (#62)" (#63)
    This reverts commit 427c9769c5b9c496c515de5afc5cc29f3538af4f.

--- modules/pkgs-config.nix
+++ modules/pkgs-config.nix
@@ -16,7 +16,12 @@ in
   nixpkgs.overlays = [ unstable-packages ];
   nixpkgs.config.allowUnfree = true;
 
-  nixpkgs.config.permittedInsecurePackages = [ ];
+  nixpkgs.config.permittedInsecurePackages = [
+    # TODO remove once bitwarden-desktop has updated electron
+    # https://github.com/NixOS/nixpkgs/issues/526914
+    # https://github.com/bitwarden/clients/pull/20448
+    "electron-39.8.10"
+  ];
 
   # set nix version manually to avoid libgit2 issue
   nix.package = pkgs.unstable.nix;
