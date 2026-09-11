fee700c635e4ca69c40703bb079b9786f963e9c6
kafu-densha <git@kafu.observer>
2026-09-10 17:08:09 -0700

    ito: backup homedir

--- hosts/ito/backup.nix
+++ hosts/ito/backup.nix
@@ -1,21 +1,27 @@
-# Uses services.restic.backups (one job per repository).
-# Each job gets its own systemd service/timer, logs, and CLI wrapper.
-#
-# CLI wrappers are added to PATH automatically:
-#   restic-backups-minecraft-onedrive snapshots
-#   restic-backups-minecraft-pc snapshots
-#
-# Manual trigger:
-#   systemctl start restic-backups-minecraft-onedrive
-#
-# View logs:
-#   journalctl -u restic-backups-minecraft-onedrive
-
 {
   config,
+  lib,
   ...
 }:
 
+let
+  backupTemplate = {
+    passwordFile = config.age.secrets.ito-restic-backup-password.path;
+    initialize = true;
+    user = "elenah";
+    timerConfig = {
+      OnCalendar = "*-*-* 03:00:00";
+      Persistent = true;
+      RandomizedDelaySec = "30min";
+    };
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 5"
+      "--keep-monthly 12"
+    ];
+    runCheck = true;
+  };
+in
 {
   age.secrets.ito-restic-backup-password = {
     owner = "elenah";
@@ -25,29 +31,26 @@
   };
 
   services.restic.backups = {
-    ito-zfs-backup = {
-      paths = [ "/data" ];
-      exclude = [
-        "/data/games"
-        "/data/backups"
-      ];
-      repository = "sftp:backup:/home/restic/ito-zfs-backup";
-      passwordFile = config.age.secrets.ito-restic-backup-password.path;
-      initialize = true;
-      user = "elenah";
-
-      timerConfig = {
-        OnCalendar = "*-*-* 04:00:00";
-        Persistent = true;
-        RandomizedDelaySec = "5min";
-      };
-
-      pruneOpts = [
-        "--keep-daily 7"
-        "--keep-weekly 5"
-        "--keep-monthly 12"
-      ];
-      runCheck = true;
-    };
+    ito-zfs-backup = lib.mkMerge [
+      {
+        paths = [ "/data" ];
+        exclude = [
+          "/data/games"
+          "/data/backups"
+        ];
+        repository = "sftp:backup:/home/restic/ito-zfs-backup";
+      }
+      backupTemplate
+    ];
+    ito-home-backup = lib.mkMerge [
+      {
+        paths = [ "/home/elenah" ];
+        exclude = [
+          "/home/elenah/.local"
+        ];
+        repository = "sftp:backup:/home/restic/ito-home-backup";
+      }
+      backupTemplate
+    ];
   };
 }
